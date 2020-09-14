e71f71c15d8a35247cd573da53967d6bb07ac44f
Daniel Berger <bergerdaniel1995@gmail.com>
2020-09-14 14:59:22 +0200

    new js try no2

--- js/start.js
+++ js/start.js
@@ -1,1 +1,28 @@
 "use strict";
+
+const _eventRef = _db.collection("Events");
+
+_eventRef.onSnapshot(function (snapshotData) {
+    let events = [];
+    snapshotData.forEach(function (doc) {
+        let event = doc.data();
+        event.id = doc.id;
+        events.push(event);
+    });
+    appendEvents(events);
+});
+
+// append events to the DOM
+function appendEvents(events) {
+    let htmlTemplate = "";
+    for (let event of events) {
+        console.log(event);
+        htmlTemplate += `
+        <article>
+        <h2>$(event.Date)</h2>
+        <h2>$(event.Location)</h2>
+        </article>
+        `;
+    }
+    document.querySelector('#movie-container').innerHTML = htmlTemplate;
+}
